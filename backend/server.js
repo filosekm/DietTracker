d4255e986d0a9aefd558d168a328348dc8e4b348
filosekm <marceli.filosek@studenci.collegiumwitelona.pl>
2025-02-24 18:35:02 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use("/api/activities", activityRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // ✅ Funkcja uruchamiająca serwer
